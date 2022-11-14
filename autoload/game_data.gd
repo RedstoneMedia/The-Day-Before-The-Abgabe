5d782861637547da5aaba7e240aca18c94b49dd4
RedstoneMedia <34373974+RedstoneMedia@users.noreply.github.com>
2022-11-14 10:59:45 +0100

    Fixed not showing tutorial/bad inital highscore

--- autoload/game_data.gd
+++ autoload/game_data.gd
@@ -4,8 +4,8 @@ var last_game_typed_text = ""
 var last_game_killed_enemies = 0
 var last_game_caffine_pickup_count = 0
 var not_a_robot = false
-var high_score = 10
-var first_time_playing = false
+var high_score = -1
+var first_time_playing = true
 
 const SETTINGS_FILENAME = "user://game_data.json"
 
